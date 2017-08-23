0069a2cb6f4680cb9f94175e43ce556d27f3c509
Artyom Polishchuk <artyuom.polishchuk@atomicus.by>
2017-08-23 16:34:15 +0300

    empty project

---
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Train();
+			var n = new Neoron(null);
+
+
+		}
+
+		private static void Train()
+		{
+			double error = 0;
+			for (int i = 0; i < 1; i++)
+			{
+
+			}
+		}
+	}
+
+
+	class Neoron
+	{
+		private readonly double[] _weghts;
+
+		public Neoron(double[] weghts)
+		{
+			_weghts = weghts;
+		}
+
+		public double CalNet(double[] inputs)
+		{
+			double sum = 0;
+			for (int i = 0; i < inputs.Length; i++)
+			{
+				sum += inputs[i] * _weghts[i];
+			}
+			return Math.Pow(1 + Math.Exp(-sum), -1);
+		}
+	}
+}
